db4177b7261ce5f7ae7776203ad7a2c04a2be5b3
jonasdettler <67739780+jonasdettler@users.noreply.github.com>
2020-07-02 17:51:14 +0200

    360

---
+++ NW/360/data.js
@@ -0,0 +1,320 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-auenperspektive",
+      "name": "Außenperspektive",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.17569039717256807,
+        "pitch": -0.0015814184741760329,
+        "fov": 1.3795342925798226
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4831447458732505,
+          "pitch": 0.12730587396723791,
+          "rotation": 0,
+          "target": "2-eingangsbereich-und-freie-arbeitssituationen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5959692432978247,
+          "pitch": 0.2012213406154153,
+          "title": "Städtebauliche Situation<br>",
+          "text": "Fassen des Vorplatzes zusammen mit den Hangars<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-bro-riegel",
+      "name": "Büro Riegel",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.3574489127273175,
+        "pitch": 0.040357666806249526,
+        "fov": 1.3795342925798226
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.259429474931844,
+          "pitch": 0.05971290138178986,
+          "rotation": 0,
+          "target": "0-auenperspektive"
+        },
+        {
+          "yaw": 0.77584941754656,
+          "pitch": -0.30809832545716986,
+          "rotation": 0,
+          "target": "4-werk--und-sporthallen"
+        },
+        {
+          "yaw": 0.907408956192473,
+          "pitch": 0.1319107174644607,
+          "rotation": 0,
+          "target": "3-multifunktionales-tal"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2598889807924021,
+          "pitch": -0.17986672222357747,
+          "title": "Gestaffelte Arbeitsplätze und Terrassen <br>",
+          "text": "Konventionelle introvertierte, sowie offene alternative Situationen<br>"
+        },
+        {
+          "yaw": 1.5077771460512448,
+          "pitch": 0.2902559017721913,
+          "title": "Innenliegende Laubengänge<br>",
+          "text": "Erschließung, Treffpunkt und Aneignung durch den Nutzer<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-eingangsbereich-und-freie-arbeitssituationen",
+      "name": "Eingangsbereich und freie Arbeitssituationen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.2507941811489136,
+        "pitch": 0.0014611395298675944,
+        "fov": 1.3795342925798226
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4418460784498084,
+          "pitch": -0.09330213748383898,
+          "rotation": 0,
+          "target": "3-multifunktionales-tal"
+        },
+        {
+          "yaw": -0.8895458731109258,
+          "pitch": 0.04500836418514531,
+          "rotation": 0,
+          "target": "0-auenperspektive"
+        },
+        {
+          "yaw": -2.2843216263543056,
+          "pitch": -0.37648693001182565,
+          "rotation": 0,
+          "target": "4-werk--und-sporthallen"
+        },
+        {
+          "yaw": -1.7199800087219916,
+          "pitch": -0.2015118742377524,
+          "rotation": 0,
+          "target": "1-bro-riegel"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.807390550724806,
+          "pitch": 0.2006918373311528,
+          "title": "Terrassierung<br>",
+          "text": "Gestufte Arbeitsplätze ermöglichen weite Blicke nach Draußen, auch aus dem Gebäudeinneren<br>"
+        },
+        {
+          "yaw": 1.6689219579245878,
+          "pitch": -0.06174158086449566,
+          "title": "Besprechungssituationen<br>",
+          "text": "Grad der Privatheit kann vom Benutzer angepasst werden<br>"
+        }
+      ]
+    },
+    {
+      "id": "3-multifunktionales-tal",
+      "name": "multifunktionales Tal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.9253729972413147,
+        "pitch": -0.27305402682173785,
+        "fov": 1.3795342925798226
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5456866607004933,
+          "pitch": -1.076698991538656,
+          "rotation": 0,
+          "target": "4-werk--und-sporthallen"
+        },
+        {
+          "yaw": -0.0008660265701667669,
+          "pitch": 0.005364607435369706,
+          "rotation": 0,
+          "target": "2-eingangsbereich-und-freie-arbeitssituationen"
+        },
+        {
+          "yaw": -1.273072007456987,
+          "pitch": -0.11147752689466017,
+          "rotation": 0,
+          "target": "0-auenperspektive"
+        },
+        {
+          "yaw": -2.1933630483816753,
+          "pitch": -0.20576309534489567,
+          "rotation": 0,
+          "target": "1-bro-riegel"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8336805422832079,
+          "pitch": 0.38556783014957396,
+          "title": "Öffentliche Nutzungen<br>",
+          "text": "Foodcourt, Playcourt, Bibliothek und Veranstaltungsfläche<br>"
+        },
+        {
+          "yaw": -0.9070315155884217,
+          "pitch": -0.5347041289896239,
+          "title": "Shortcut System<br>",
+          "text": "Tubes zur schnellen vertikalen und horizontalen Erschließung<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-werk--und-sporthallen",
+      "name": "Werk- und Sporthallen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.15842219364257382,
+        "pitch": 0.012997183363166798,
+        "fov": 1.3795342925798226
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.014364836464338282,
+          "pitch": 0.3061010257229917,
+          "rotation": 0,
+          "target": "2-eingangsbereich-und-freie-arbeitssituationen"
+        },
+        {
+          "yaw": -0.9889035010848239,
+          "pitch": -0.8754002411458472,
+          "rotation": 0,
+          "target": "0-auenperspektive"
+        },
+        {
+          "yaw": -1.3454097289499707,
+          "pitch": 0.004738121826511232,
+          "rotation": 0,
+          "target": "1-bro-riegel"
+        },
+        {
+          "yaw": 0.5698215662273398,
+          "pitch": 0.6387746653844815,
+          "rotation": 0,
+          "target": "3-multifunktionales-tal"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4977370913463748,
+          "pitch": -0.01955323859591651,
+          "title": "Nutzungen mit hohem Platzbedarf<br>",
+          "text": "Große, zwischen die Büroriegel eingehängte Volumen, vertikal untereinander verschoben um interessante Blicke zu generieren<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
